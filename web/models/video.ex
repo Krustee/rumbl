f0d0b79c575c5fffcc26790172399afb49c30b59
Lars Jensen <larsjensen87@gmail.com>
2016-07-03 00:40:08 +0200

    Chapter 9: Watching Videos

--- web/models/video.ex
+++ web/models/video.ex
@@ -1,5 +1,6 @@
 defmodule Rumbl.Video do
   use Rumbl.Web, :model
+  @primary_key {:id, Rumbl.Permalink, autogenerate: true}
 
   @required_fields ~w(url title description)a
   @optional_fields ~w(category_id)a
@@ -8,6 +9,7 @@ defmodule Rumbl.Video do
     field :url, :string
     field :title, :string
     field :description, :string
+    field :slug, :string
     belongs_to :user, Rumbl.User
     belongs_to :category, Rumbl.Category
 
@@ -17,10 +19,31 @@ defmodule Rumbl.Video do
   @doc """
   Builds a changeset based on the `struct` and `params`.
   """
-  def changeset(struct, params \\ %{}) do
+  def changeset(struct, params \\ :invalid) do
     struct
     |> cast(params, @required_fields, @optional_fields)
     |> validate_required(@required_fields)
+    |> slugify_title
     |> assoc_constraint(:category)
+  end
+  
+  defp slugify_title(changeset) do
+    if title = get_change(changeset, :title) do
+      put_change(changeset, :slug, slugify(title))
+    else
+      changeset
+    end
+  end
+  
+  defp slugify(str) do
+    str
+    |> String.downcase
+    |> String.replace(~r/[^\w-]+/u, "-")
+  end
+  
+  defimpl Phoenix.Param, for: Rumbl.Video do
+    def to_param(%{slug: slug, id: id}) do
+      "#{id}-#{slug}"
+    end
   end
 end
